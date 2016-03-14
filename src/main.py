6601b7ab1a4f3c3bb9da8343d63a76182ed12aaa
pchmiel <piotr.chmiel@hotmail.com>
2016-03-14 22:30:44 +0100

    Small update

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from os import path
 from pprint import pprint
 from src.utils import ExcelParser
@@ -7,7 +8,10 @@ from src.settings import TRAINING_SET_DIR, TRAINING_SET_FILENAME, SHEET_NAME
 def main():
     parser = ExcelParser(path.join(TRAINING_SET_DIR, TRAINING_SET_FILENAME), SHEET_NAME)
 
-    rows = [row for row in parser.get_rows()]
+    rows = defaultdict(list)
+    for row in parser.get_rows():
+        rows[row['wy']].append(row)
+
     pprint(rows)
 
 if __name__ == '__main__':
