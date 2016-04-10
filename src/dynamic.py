cb05a503955c98dd26b6b55320ce904197bb0938
Jakub <jagub2@users.noreply.github.com>
2016-04-10 19:41:22 +0200

    add dynamic one-vs-one classificator

--- src/dynamic.py
+++ src/dynamic.py
@@ -1,4 +1,7 @@
-from sklearn.multiclass import OneVsOneClassifier
+from cmath import sqrt
+import numpy as np
+
+from sklearn.multiclass import OneVsOneClassifier, _predict_binary, _ovr_decision_function
 from sklearn.neighbors import NearestNeighbors
 
 
@@ -12,13 +15,29 @@ class DynamicOneVsOneClassifier(OneVsOneClassifier):
                                      metric=metric, p=p, metric_params=metric_params, n_jobs=n_jobs)
         self.n_neighbors = n_neighbors
         self._fit_X = None
+        self._fit_Y = None
 
     def fit(self, X, y):
+        self._fit_Y = y
         self._fit_X = self.nbrs.fit(X, y)
         return OneVsOneClassifier.fit(self, X, y)
 
     def decision_function(self, X):
         neighbors = self.nbrs.kneighbors(X, self.n_neighbors, return_distance=False)
-        #print(neighbors)
-        # neighbors zawiera numery wierszy w self._fit_x trzeba wziąć ich klasy spakować do seta
-        return OneVsOneClassifier.decision_function(self, X)
+        estimators_set = set([])
+        estimators = list()
+        for neighbor in neighbors[0]:
+            estimators_set.add(self._fit_Y[neighbor])
+
+        n_classes = int(((1 + sqrt(4 * 2 * len(self.estimators_) + 1)) / 2).real)  # n*(n-1)/2 binary classificators
+
+        k = 0
+        for i in range(n_classes):
+            for j in range(i + 1, n_classes):
+                if i in estimators_set or j in estimators_set:
+                    estimators.append(self.estimators_[k])
+                k += 1
+
+        predictions = np.vstack([est.predict(X) for est in estimators]).T
+        confidences = np.vstack([_predict_binary(est, X) for est in estimators]).T
+        return _ovr_decision_function(predictions, confidences, len(estimators_set))
