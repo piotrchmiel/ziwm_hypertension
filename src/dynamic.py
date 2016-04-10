04ea7a99cd08aac1bf637d95a5587dd23d01eeda
Jakub <jagub2@users.noreply.github.com>
2016-04-10 21:36:07 +0200

    fix dynamic OvO decision function to work as intended

--- src/dynamic.py
+++ src/dynamic.py
@@ -1,8 +1,57 @@
-from cmath import sqrt
+from cmath import sqrt, isnan
 import numpy as np
 
-from sklearn.multiclass import OneVsOneClassifier, _predict_binary, _ovr_decision_function
+from sklearn.multiclass import OneVsOneClassifier, _predict_binary
 from sklearn.neighbors import NearestNeighbors
+
+
+def dynamic_ovr_decision_function(predictions, confidences, n_classes):
+    """Compute a continuous, tie-breaking ovr decision function.
+
+    It is important to include a continuous value, not only votes,
+    to make computing AUC or calibration meaningful.
+
+    Parameters
+    ----------
+    predictions : array-like, shape (n_samples, n_classifiers)
+        Predicted classes for each binary classifier.
+
+    confidences : array-like, shape (n_samples, n_classifiers)
+        Decision functions or predicted probabilities for positive class
+        for each binary classifier.
+
+    n_classes : int
+        Number of classes. n_classifiers must be
+        ``n_classes * (n_classes - 1 ) / 2``
+    """
+    n_samples = predictions.shape[0]
+    votes = np.zeros((n_samples, n_classes))
+    sum_of_confidences = np.zeros((n_samples, n_classes))
+
+    k = 0
+    for i in range(n_classes):
+        for j in range(i + 1, n_classes):
+            if not isnan(confidences[:, k]) or not isnan(predictions[:, k]):
+                sum_of_confidences[:, i] -= confidences[:, k]
+                sum_of_confidences[:, j] += confidences[:, k]
+                votes[predictions[:, k] == 0, i] += 1
+                votes[predictions[:, k] == 1, j] += 1
+            k += 1
+
+    max_confidences = sum_of_confidences.max()
+    min_confidences = sum_of_confidences.min()
+
+    if max_confidences == min_confidences:
+        return votes
+
+    # Scale the sum_of_confidences to (-0.5, 0.5) and add it with votes.
+    # The motivation is to use confidence levels as a way to break ties in
+    # the votes without switching any decision made based on a difference
+    # of 1 vote.
+    eps = np.finfo(sum_of_confidences.dtype).eps
+    max_abs_confidence = max(abs(max_confidences), abs(min_confidences))
+    scale = (0.5 - eps) / max_abs_confidence
+    return votes + sum_of_confidences * scale
 
 
 class DynamicOneVsOneClassifier(OneVsOneClassifier):
@@ -36,8 +85,10 @@ class DynamicOneVsOneClassifier(OneVsOneClassifier):
             for j in range(i + 1, n_classes):
                 if i in estimators_set or j in estimators_set:
                     estimators.append(self.estimators_[k])
+                else:
+                    estimators.append(None)
                 k += 1
 
-        predictions = np.vstack([est.predict(X) for est in estimators]).T
-        confidences = np.vstack([_predict_binary(est, X) for est in estimators]).T
-        return _ovr_decision_function(predictions, confidences, len(estimators_set))
+        predictions = np.vstack([est.predict(X) if est is not None else np.nan for est in estimators]).T
+        confidences = np.vstack([_predict_binary(est, X) if est is not None else np.nan for est in estimators]).T
+        return dynamic_ovr_decision_function(predictions, confidences, len(self.classes_))
