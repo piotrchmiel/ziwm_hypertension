dfb01575f304b771c94ddd401104f7a7435b464e
pchmiel <piotr.chmiel@hotmail.com>
2016-04-10 16:17:07 +0200

    Classifier Creator Parallel

--- src/classifier_creator.py
+++ src/classifier_creator.py
@@ -1,5 +1,6 @@
 from os import path
 
+from joblib import Parallel, delayed
 from sklearn.tree import DecisionTreeClassifier
 
 from src.factories import LearningSetFactory, MulticlassClassifierFactory
@@ -7,18 +8,16 @@ from src.settings import CLASSIFIERS_DIR, TRAINING_SET_DIR, ALGORITHMS, METHODS
 from src.utils import save_object
 
 
-def create_classifiers(method, train_set, train_labels, *args, **kwargs):
-    for name, algorithm_class in ALGORITHMS.items():
-        if name in METHODS[method]:
-            multiclass_classifier = MulticlassClassifierFactory.make_default_classifier(algorithm_class, train_set,
-                                                                                        train_labels, *args, **kwargs)
-            save_object(path.join(CLASSIFIERS_DIR, "".join(['multiclass_', name, '.pickle'])), multiclass_classifier)
+def create_classifiers(name, algorithm_class, train_set, train_labels, *args, **kwargs):
+    multiclass_classifier = MulticlassClassifierFactory.make_default_classifier(algorithm_class, train_set,
+                                                                                train_labels, *args, **kwargs)
+    save_object(path.join(CLASSIFIERS_DIR, "".join(['multiclass_', name, '.pickle'])), multiclass_classifier)
 
-            two_layer_classifier = MulticlassClassifierFactory.make_default_two_layer_classifier(algorithm_class,
-                                                                                                 train_set,
-                                                                                                 train_labels, *args,
-                                                                                                 **kwargs)
-            save_object(path.join(CLASSIFIERS_DIR, "".join(['two_layer_', name, '.pickle'])), two_layer_classifier)
+    two_layer_classifier = MulticlassClassifierFactory.make_default_two_layer_classifier(algorithm_class,
+                                                                                         train_set,
+                                                                                         train_labels, *args,
+                                                                                         **kwargs)
+    save_object(path.join(CLASSIFIERS_DIR, "".join(['two_layer_', name, '.pickle'])), two_layer_classifier)
 
 
 def main():
@@ -34,9 +33,13 @@ def main():
 
     keywords_ensemble = {'n_estimators': 50}
     keywords_multiclass = {'n_jobs': -1}  # -1 means: use all CPUs
-
-    create_classifiers('ensemble', train_set, train_labels, **keywords_ensemble)
-    create_classifiers('multiclass', train_set, train_labels, DecisionTreeClassifier(), **keywords_multiclass)
+    Parallel(n_jobs=-1)(delayed(create_classifiers)
+                        (name, algorithm_class, train_set, train_labels, **keywords_ensemble)
+                        for name, algorithm_class in ALGORITHMS.items() if name in METHODS['ensemble'])
+    Parallel(n_jobs=-1)(delayed(create_classifiers)
+                        (name, algorithm_class, train_set, train_labels,
+                         DecisionTreeClassifier(), **keywords_multiclass)
+                        for name, algorithm_class in ALGORITHMS.items() if name in METHODS['multiclass'])
 
     print("Done.")
 
