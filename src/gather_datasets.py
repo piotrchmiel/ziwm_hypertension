c4f8feae3e17947e034e809173ff1ff79a41b67f
Jakub <jagub2@users.noreply.github.com>
2016-04-14 21:49:03 +0200

    generate csv for auslan dataset, add header when concatenating

--- src/gather_datasets.py
+++ src/gather_datasets.py
@@ -1,5 +1,6 @@
-from os import remove
-from os.path import dirname, isfile, join as pjoin, split, splitext, realpath
+from glob import glob
+from os import sep, remove
+from os.path import dirname, exists, join as pjoin, split, splitext, realpath
 from urllib.request import urlretrieve
 from shutil import move
 from patoolib import extract_archive
@@ -25,19 +26,19 @@ datasets = {
         'download': [
             'http://archive.ics.uci.edu/ml/machine-learning-databases/auslan2-mld/tctodd.tar.gz'
         ],
-        'files': {},
-        'operation': 'nop',
-        'out': ''
+        'files': ['tctodd' + str(i) for i in range(1, 10)],
+        'operation': 'auslan_concat',
+        'out': 'auslan.csv'
     },
     'isolet': {
         'download': [
             'http://archive.ics.uci.edu/ml/machine-learning-databases/isolet/isolet1+2+3+4.data.Z',
             'http://archive.ics.uci.edu/ml/machine-learning-databases/isolet/isolet5.data.Z'
         ],
-        'files': {
+        'files': [
             'isolet1+2+3+4.data',
             'isolet5.data'
-        },
+        ],
         'operation': 'concat',
         'out': 'isolet.csv'
     },
@@ -45,7 +46,7 @@ datasets = {
         'download': [
             'http://sci2s.ugr.es/keel/dataset/data/classification/abalone.zip'
         ],
-        'files': {'abalone.dat'},
+        'files': ['abalone.dat'],
         'operation': 'dat2csv',
         'out': 'abalone.csv'
     },
@@ -53,7 +54,7 @@ datasets = {
         'download': [
             'http://sci2s.ugr.es/keel/dataset/data/classification/letter.zip'
         ],
-        'files': {'letter.dat'},
+        'files': ['letter.dat'],
         'operation': 'dat2csv',
         'out': 'letter.csv'
     },
@@ -61,7 +62,7 @@ datasets = {
         'download': [
             'http://sci2s.ugr.es/keel/dataset/data/classification/kddcup.zip'
         ],
-        'files': {'kddcup.dat'},
+        'files': ['kddcup.dat'],
         'operation': 'dat2csv',
         'out': 'kddcup.csv'
     },
@@ -76,49 +77,73 @@ def main():
         for link in items['download']:
             filename = link.split('/')[-1]
             archive = pjoin(training_root, filename)
-            if not isfile(archive):
+            if not exists(archive):
                 print('Downloading', filename)
                 urlretrieve(link, archive)
 
         # extract files
-        for file in items['files']:
-            if not isfile(pjoin(training_root, file)):
-                links = [d for d in items['download'] if splitext(file)[0] in d]
+        for out_file in items['files']:
+            if not exists(pjoin(training_root, out_file)):
+                links = [d for d in items['download'] if splitext(out_file)[0] in d]
                 if len(links) > 0:
                     filename = links[0].split('/')[-1]
                     archive = pjoin(training_root, filename)
-                    if not file.endswith('.tar.gz'):
-                        extract_archive(archive, outdir=dirname(archive), verbosity=-1, interactive=False)
-                    else:
-                        extract_archive(archive, outdir=dirname(archive), program='py_tarfile',
-                                        verbosity=-1, interactive=False)
+                else:
+                    archive = pjoin(training_root, items['download'][0].split('/')[-1])
+                print('Extracting', archive)
+                if archive.endswith('.tar.gz'):
+                    extract_archive(archive, outdir=dirname(archive), program='py_tarfile',
+                                    verbosity=-1, interactive=False)
+                else:
+                    extract_archive(archive, outdir=dirname(archive), verbosity=-1, interactive=False)
 
         # generate output files
         if type(items['files']) == dict:
+            print('Renaming files')
             for source, destination in items['files'].items():
                 move(pjoin(training_root, source), pjoin(training_root, destination))
         out_filename = pjoin(training_root, items['out'])
-        if items['operation'] == 'nop':
-            pass
-        elif isfile(out_filename):
+        print('Generating output files')
+        if items['operation'] == 'auslan_concat':
+            first_record = True
+            with open(out_filename, 'w') as out_file:
+                for filename in glob(pjoin(training_root, 'tctodd*', '*')):
+                    class_ = filename.split(sep)[-1].split('-')[0]
+                    with open(pjoin(training_root, filename)) as infile:
+                        for line in infile:
+                            if first_record:
+                                header = ';'.join(
+                                    ['Atr-' + str(i) for i in range(1, len(line.split('\t')))]
+                                ) + ';Class'
+                                out_file.write(header + '\n')
+                                first_record = False
+                            out_file.write(line.replace('\t', ';').rstrip() + ';' + class_ + '\n')
+        elif not exists(out_filename):
             if items['operation'] == 'concat' or items['operation'] == 'dat2csv':
-                with open(out_filename, 'w') as file:
+                first_record = True
+                with open(out_filename, 'w') as out_file:
                     for fname in items['files']:
                         with open(pjoin(training_root, fname)) as infile:
                             for line in infile:
-                                file.write(line.replace(', ', ';').replace(',', ';'))
+                                if first_record and items['operation'] == 'concat':
+                                    first_record = False
+                                    header = ';'.join(
+                                        ['Atr-' + str(i) for i in range(1, len(line.split(', ')))]
+                                    ) + ';Class'
+                                    out_file.write(header + '\n')
+                                out_file.write(line.replace(', ', ';').replace(',', ';'))
             if items['operation'] == 'dat2csv':
-                with open(out_filename, 'r+') as file:
+                with open(out_filename, 'r+') as out_file:
                     with open(out_filename + '.tmp', 'w') as new_file:
                         skip_contents = True
                         header = ''
-                        for line in file:
+                        for line in out_file:
                             if not '@data' in line:
                                 if not skip_contents:
                                     new_file.write(line)
                                 else:
                                     if '@inputs' in line or '@outputs' in line:
-                                        header += line.split(' ')[1].rstrip() + ";"
+                                        header += line.split(' ')[1].rstrip() + ';'
                             else:
                                 skip_contents = False
                                 new_file.write(header[:-1] + '\n')
