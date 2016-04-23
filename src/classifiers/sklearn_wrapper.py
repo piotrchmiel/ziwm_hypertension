3af8ad1e311762d5c5028d9511bf90f3efe499fe
pchmiel <piotr.chmiel@hotmail.com>
2016-04-23 15:15:48 +0200

    Refactoring + new sets from mldata

--- src/classifiers/sklearn_wrapper.py
+++ src/classifiers/sklearn_wrapper.py
@@ -36,7 +36,7 @@ class SklearnWrapper(object):
             pred = [self.classify(feature_set) for feature_set in X]
             return accuracy_score(y, pred) * 100
         else:
-            return self._clf_pipeline.predict.score(X, y)
+            return self._clf_pipeline.score(X, y)
 
     def get_classifier(self):
         return self._clf_pipeline
